86b5cabff5634f44bbf186579a9baeda081410b9
VirtualStein <ayushrajguptagn@gmail.com>
2023-04-25 05:40:10 +0530

    Added Score system

--- TxtRPG.java
+++ TxtRPG.java
@@ -15,7 +15,9 @@ public class TxtRPG
         int enemyAttackDamage = 25;
 
         //Player variables
+        String name = "Player";
         int health = 100;
+        int score = 0;
         int attackDamage = 50;
         int numHealthPotions = 3;
         int healthPotionHealAmount = 30;
@@ -31,91 +33,107 @@ public class TxtRPG
             System.out.println("\n\t\t!!! Welcome to the Dungeon !!!\n");
             
             //Start of the Game
+            System.out.println("------------------------------------------------------------\n"); //Added dashed line for better UI
+            System.out.print("Enter Player's Name: \t");
+            name = in.nextLine();
             
             //Main loop
             GAME:               //This is a 'GAME' label named to the while loop. So that we can iterate back when needed.
             while(running)      //This is the loop which will keep iterating to keep the game going.
             {
-                System.out.println("------------------------------------------------------------\n"); //Added dashed line for better UI
+                System.out.println("\n------------------------------------------------------------\n"); //Added dashed line for better UI
                 
                 //Introducing a random enemy
                 int enemyHealth = rand.nextInt(maxEnemyHealth);
                 String enemy = enemies[rand.nextInt(enemies.length)];
                 System.out.println("\t#\tA "+ enemy + " has appeared!      #\n");
                 
+                //generates random score number
+                int genscore = rand.nextInt(1,enemies.length); 
+                // System.out.println(genscore);
+                
                 //Loop till the enemy's HP is 0 
                 while(enemyHealth > 0)
                 {
                     //Platform Details and Choices to play game
-                    System.out.println("\t\tPlayer HP: " + health);
+                    System.out.println("\n\t\t"+name+"'s HP: " + health);
                     System.out.println("\t\t" + enemy + "'s HP: " + enemyHealth);
-                System.out.println("\n\tWhat would you like to do?");
-                System.out.println("\t 1. ATTACK!!!");
-                System.out.println("\t 2. Drink Health Potion");
-                System.out.println("\t 3. Evade! :| ");
-                
-                //Taking Input from User and implementing accordingly
-                String input = in.nextLine();
-                if (input.equals("1")) 
-                {
-                    int damageDealt = rand.nextInt(attackDamage);       //Generating random attack damge of player
-                    int damageTaken = rand.nextInt(enemyAttackDamage);      //Generating random attack damge of enemy
+                    System.out.println("\n\tWhat would you like to do?");
+                    System.out.println("\t 1. ATTACK!!!");
+                    System.out.println("\t 2. Drink Health Potion");
+                    System.out.println("\t 3. Evade! :| ");
                     
-                    //Reducing both HPs accordingly
-                    enemyHealth -= damageDealt;
-                    health -= damageTaken;
-
-                    //Printing post-fight details
-                    System.out.println("> You striked the "+enemy+" for "+damageDealt+" damage.");
-                    System.out.println("> You received the "+damageTaken+" damage in retaliation.\n");
-                    
-                    //Breaking loop at Low Health alert
-                    if (health < 1) 
+                    //Taking Input from User and implementing accordingly
+                    String input = in.nextLine();
+                    if (input.equals("1")) 
                     {
-                        System.out.print("\n\t\tLOW HEALTH\t\t\n");
-                        System.out.println(">You have taken too much damage, you are too weak to go on!"); 
-                        break;  
+                        int damageDealt = rand.nextInt(attackDamage);       //Generating random attack damge of player
+                        int damageTaken = rand.nextInt(enemyAttackDamage);      //Generating random attack damge of enemy
+                        
+                        //Reducing both HPs accordingly
+                        enemyHealth -= damageDealt;
+                        health -= damageTaken;
+
+                        //Printing post-fight details
+                        System.out.println("------------------------------------------------------------"); //Added dashed line for better UI
+                        System.out.println("> You striked the "+enemy+" for "+damageDealt+" damage.");
+                        System.out.println("> You received the "+damageTaken+" damage in retaliation.");
+                        System.out.println("------------------------------------------------------------"); //Added dashed line for better UI
+                        
+                        //Breaking loop at Low Health alert
+                        if (health < 1) 
+                        {
+                            System.out.print("\n\t\tLOW HEALTH\t\t\n");
+                            System.out.println(">You have taken too much damage, you are too weak to go on!"); 
+                            break;  
+                        }
                     }
-                }
-                else if (input.equals("2")) 
-                {
-                    //Applying available Health Boost
-                    if (numHealthPotions > 0) {
-                        health += healthPotionHealAmount;
-                        numHealthPotions--;
-                        System.out.println("\t> You drink a health potion , healing yourself for "+healthPotionHealAmount+" HP.");      //Printing Received HP
-                        System.out.println("n\t> You now have "+health+" HP.");     //Printing Total HP
-                        System.out.println("\n\t> You have "+numHealthPotions+" health potions left.\n");       //Printing Health Potions Left
+                    else if (input.equals("2")) 
+                    {
+                        //Applying available Health Boost
+                        if (numHealthPotions > 0) {
+                            health += healthPotionHealAmount;
+                            numHealthPotions--;
+                            System.out.println("------------------------------------------------------------"); //Added dashed line for better UI
+                            System.out.println("\n\t> You drink a health potion , healing yourself for "+healthPotionHealAmount+" HP.");      //Printing Received HP
+                            System.out.println("\t> You now have "+health+" HP.");     //Printing Total HP
+                            System.out.println("\t> You have "+numHealthPotions+" health potions left.\n");       //Printing Health Potions Left
+                            System.out.println("------------------------------------------------------------"); //Added dashed line for better UI
+                        }
+                        //Health Boost Unavailable
+                        else
+                        {
+                            System.out.println("\n\t> You have no health potions left! \n\t>Defeat enemies for a chance to get one!!\n");       //Printing Health Potions Left
+                        }
                     }
-                    //Health Boost Unavailable
+                    else if (input.equals("3")) 
+                    {
+                        //Printing Evade from enemy
+                        System.out.println("\n\t> You evaded away from the "+enemy+" !");
+                        continue GAME;          //recurses back to Main loop
+                    }
                     else
                     {
-                        System.out.println("\n\t> You have no health potions left! \n\t>Defeat enemies for a chance to get one!!\n");       //Printing Health Potions Left
+                        //Default for negating Invalid Input
+                        System.out.println("\n\t    #       Invalid Command     #");
                     }
-                }
-                else if (input.equals("3")) 
-                {
-                    //Printing Evade from enemy
-                    System.out.println("\n\t> You evaded away from the "+enemy+" !");
-                    continue GAME;          //recurses back to Main loop
-                }
-                else
-                {
-                    //Default for negating Invalid Input
-                    System.out.println("\n\t    #       Invalid Command     #");
-                }
             }
             
             if(health< 1)
             {
-                System.out.println("\t You limp out of the dungeon, weak from battle.");
+                System.out.println("\n\t You limp out of the dungeon, weak from battle.");
                 break;
             } 
             
-            System.out.println("------------------------------------------------------------\n"); //Added dashed line for better UI
+            // System.out.println("------------------------------------------------------------\n"); //Added dashed line for better UI
             
-            System.out.println("\t# "+enemy+" was DEFEATED !! \t#");
-            System.out.println("\n\t# You have "+health+" HP left ! #");
+            //Printing Player's Stats
+            System.out.println("\n\t#       "+enemy+" was DEFEATED !!\t#");
+            System.out.println("\n\t#       You have "+health+" HP left !\t#");
+            score += genscore;      //Incrensing Score
+            System.out.println("\n\t#       Your Score is : "+score+"\t#\n");
+
+            //Health Potion Drop
             if (rand.nextInt(100) < healthPotionDropChance) {
                 numHealthPotions++;
                 System.out.println("\t# The "+enemy+" dropped a health potion! #");
@@ -147,9 +165,9 @@ public class TxtRPG
             }
         
             //Closure Line
-            System.out.println("\t########################");
-            System.out.println("\t# THANKS FOR PLAYING ! #");
-            System.out.println("\t########################");
+            System.out.println("\t\t########################");
+            System.out.println("\t\t# THANKS FOR PLAYING ! #");
+            System.out.println("\t\t########################");
             
             System.out.println("------------------------------------------------------------\n"); //Added dashed line for better UI
             
@@ -162,12 +180,12 @@ public class TxtRPG
         
         if (play == false ) 
         {
-            System.out.println("\t#####################");
-            System.out.println("\t# COME BACK AGAIN ! #");
-            System.out.println("\t#####################");
+            System.out.println("\t\t#####################");
+            System.out.println("\t\t# COME BACK AGAIN ! #");
+            System.out.println("\t\t#####################");
             
         }
-        System.out.println("Press any key to continue ..................");
+        System.out.println("Press any key to continue..................................................................................");
         in.nextLine();
         in.close();
     }    
